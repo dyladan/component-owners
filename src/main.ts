db64377ea285b5c13fe2929029975ae6313b044d
Daniel Dyla <dyladan@users.noreply.github.com>
2021-08-06 11:15:08 -0400

    Make a change to an owned file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { getChangedFiles, getConfig, getOwners, getPullAuthor, getRefs, getCollaboratorLogins } from "./utils";
 
+// this file is owned by dyladan
+
 async function main() {
     const client = github.getOctokit(core.getInput('repo-token', { required: true }));
     const ownerFilePath = core.getInput('config-file', { required: true });
